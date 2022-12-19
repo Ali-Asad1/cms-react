c05ca19fa6aef100b339ffdc9594c22609c3b755
Ali <aliasad.3128@gmail.com>
2022-12-19 11:45:53 +0330

    add custom.css (fonts , colors)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './reset.css'
 import './index.css';
+import './custom.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
