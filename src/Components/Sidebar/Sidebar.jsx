fc3625528cdb9ff6f9b10360311c9b52d680e34c
Ali <aliasad.3128@gmail.com>
2022-12-19 19:20:06 +0330

    update sidebar (icon , hover animation)

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
 import './Sidebar.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { BsCart3, BsBagCheck , BsCurrencyDollar } from 'react-icons/bs'
+import { BiCommentDetail, BiDollar } from 'react-icons/bi'
+import { FiUsers } from 'react-icons/fi'
+import { IoPricetagsOutline } from 'react-icons/io5'
 export default function Sidebar() {
     return (
         <div className='sidebar'>
             <h2 className="sidebar-title">به داشبورد خوش آمدید</h2>
             <ul className="sidebar-links">
                 <li className='active'>
-                    <a href="#">صحفه اصلی</a>
+                    <a href="#"><AiOutlineHome /> صحفه اصلی</a>
                 </li>
                 <li>
-                    <a href="#">محصولات</a>
+                    <a href="#"><BsCart3 /> محصولات</a>
                 </li>
                 <li>
-                    <a href="#">کامنت ها</a>
+                    <a href="#"><BiCommentDetail /> کامنت ها</a>
                 </li>
                 <li>
-                    <a href="#">کاربران</a>
+                    <a href="#"><FiUsers /> کاربران</a>
                 </li>
                 <li>
-                    <a href="#">سفارشات</a>
+                    <a href="#"><BsBagCheck /> سفارشات</a>
                 </li>
                 <li>
-                    <a href="#">تخفیف ها</a>
+                    <a href="#"><IoPricetagsOutline /> تخفیف ها</a>
                 </li>
+                <div className="underline"></div>
             </ul>
         </div>
     )
