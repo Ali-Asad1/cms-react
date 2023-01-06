10c654f573453fe7c07215d64c8087390f532c29
Ali Asad <aliasad.3128@gmail.com>
2023-01-06 18:04:05 +0330

    add admin info with token authorization

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import { useRoutes } from 'react-router-dom'
-import { routes } from './routes'
-import Sidebar from './Components/Sidebar/Sidebar'
-import Header from './Components/Header/Header'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { useRoutes } from "react-router-dom";
+import { routes } from "./routes";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Header from "./Components/Header/Header";
+import "./App.css";
 
 export default function App() {
-  const router = useRoutes(routes)
-  return (
-    <div className='app'>
-      <Sidebar />
-      <div className="main">
-        <Header />
-        {router}
-      </div>
-    </div>
-  )
+    const [adminInfo, setAdminInfo] = useState(null);
+    useEffect(() => {
+        let token = localStorage.getItem("admin-token");
+        fetch("http://localhost:8000/api/admins", {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: token,
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setAdminInfo(data);
+            });
+    }, []);
+    const router = useRoutes(routes);
+    return (
+        <div className='app'>
+            <Sidebar />
+            <div className='main'>
+                <Header adminInfo={adminInfo} />
+                {router}
+            </div>
+        </div>
+    );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import './Header.css'
-import { BsFillBrightnessHighFill , BsFillBellFill } from 'react-icons/bs'
-export default function Header() {
+import React from "react";
+import "./Header.css";
+import { BsFillBrightnessHighFill, BsFillBellFill } from "react-icons/bs";
+export default function Header({ adminInfo }) {
     return (
         <div className='header'>
-            <div className="admin-profile">
-                <img src="./images/sample-user.png" alt="" />
+            <div className='admin-profile'>
+                <img src={adminInfo ? adminInfo.img : "./images/sample-user.png"} alt='' />
                 <div>
-                    <h2>علی اسعد</h2>
-                    <h3>برنامه نویس فرانت</h3>
+                    <h2>
+                        {adminInfo ? `${adminInfo.firstname}  ${adminInfo.lastname}` : "ناشناس"}
+                    </h2>
+                    <h3>{adminInfo ? adminInfo.task : "ناشناس"}</h3>
                 </div>
             </div>
-            <div className="header-left">
-                <div className="search-box">
-                    <input type="text" placeholder='سرچ کنید ...' />
+            <div className='header-left'>
+                <div className='search-box'>
+                    <input type='text' placeholder='سرچ کنید ...' />
                     <button id='search-btn'>جست و جو</button>
                 </div>
-                <button className="header-icon">
+                <button className='header-icon'>
                     <BsFillBellFill />
                 </button>
-                <button className="header-icon">
+                <button className='header-icon'>
                     <BsFillBrightnessHighFill />
                 </button>
             </div>
         </div>
-    )
+    );
 }
